6d54b2b6a7779fa9a1c6fdf2ec99e4307cd97101
Yash Patel <yashpatel@Yashs-MacBook-Pro.local>
2024-09-24 11:28:31 -0400

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `csc496project`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
