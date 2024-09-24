2e40578f98c17b2a3a7d25770e072b77a18251b0
Yash Patel <yashpatel@Yashs-MacBook-Pro.local>
2024-09-24 14:34:23 -0400

    add about.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ const IndexPage = () => {
   return (
     <main>
       <h1>Welcome to my Gatsby site!</h1>
-      <p>I'm making this by following the Gatsby Tutorial.</p>
+      <p>My name is yash patel.</p>
     </main>
   )
 }
