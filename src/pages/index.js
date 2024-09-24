e607e12beb1cc2ed2324bc3ed60979e3b69a82f8
Yash Patel <yashpatel@Yashs-MacBook-Pro.local>
2024-09-24 15:44:09 -0400

    add component.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,16 @@
-// Step 1: Import React
 import * as React from 'react'
+import { Link } from 'gatsby'
 
-// Step 2: Define your component
 const IndexPage = () => {
   return (
     <main>
       <h1>Welcome to my Gatsby site!</h1>
-      <p>My name is yash patel.</p>
+      <Link to="/about">About</Link>
+      <p>I'm making this by following the Gatsby Tutorial.</p>
     </main>
   )
 }
 
-// You'll learn about this in the next task, just copy it for now
 export const Head = () => <title>Home Page</title>
 
-// Step 3: Export your component
 export default IndexPage
